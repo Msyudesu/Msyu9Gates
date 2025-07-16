d2ee3673b8007e50733294ffad77264a356dc8d4
Msyu Desu <msyudesu@gmail.com>
2025-07-16 03:18:23 -0400

    msyu/gate3:  tweaks

---
+++ Msyu9Gates/Msyu9Gates/Data/Models/KeyModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Msyu9Gates.Data.Models
+{
+    public class KeyModel
+    {
+        public int Id { get; set; }
+        public string? KeyValue { get; set; }
+        public bool Discovered { get; set; } = false;
+        public DateTime DateDiscovered { get; set; }
+    }
+}
