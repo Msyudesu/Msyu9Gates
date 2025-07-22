f0293869ccd49e0d4e7a406b5952076269440a62
Msyu Desu <msyudesu@gmail.com>
2025-07-22 14:37:21 -0400

    refactor + added Gate3Chapter3Narrative to the file manifest

--- Msyu9Gates/Msyu9Gates/Program.cs
+++ Msyu9Gates/Msyu9Gates/Program.cs
@@ -7,6 +7,7 @@ using Msyu9Gates.Data;
 using Msyu9Gates.Data.Models;
 using Msyu9Gates.Lib;
 using Msyu9Gates.Utils;
+using System;
 using System.Drawing.Text;
 using System.Security.Cryptography;
 
@@ -40,53 +41,7 @@ namespace Msyu9Gates
 
             var environment = app.Environment;
 
-
-            try
-            {
-                using (var scope = app.Services.CreateScope())
-                {
-                    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                    db.Database.Migrate(); // Applies existing migrations from the Migrations folder. Be sure to build and commit migrations before deploying.
-                                           // DO NOT COMMIT DATABASE FILES.  .db, .db-wal, and .db-shm (added to gitignore)
-                    logger.LogInformation($"Database Migration Completed in: Running in {environment}");
-                }
-                BuildGatesAndRegisterApis(app, builder);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "An error occurred during database migration or data rebuild. Deleting Database file.");
-
-                string db_path = Path.Combine(app.Environment.ContentRootPath, "msyu9gates.db");
-                string db_wal_path = Path.Combine(app.Environment.ContentRootPath, "msyu9gates.db-wal");
-                string db_shm_path = Path.Combine(app.Environment.ContentRootPath, "msyu9gates.db-shm");
-
-                if (File.Exists(db_path))
-                {
-                    File.Delete(db_path);
-                    logger.LogWarning("Database file found: msyu9gates.db -- Deleting.");
-                }
-                if(File.Exists(db_wal_path))
-                {
-                    File.Delete(db_wal_path);
-                    logger.LogWarning("Database WAL file found: msyu9gates.db-wal -- Deleting.");
-                }
-                if (File.Exists(db_shm_path))
-                {
-                    File.Delete(db_shm_path);
-                    logger.LogWarning("Database SHM file found: msyu9gates.db-shm -- Deleting.");
-                }
-
-                if(args.Length == 0)
-                {
-                    logger.LogError("Retrying migration after deleting database files.");
-                    Main(new string[1] { "-retry" }); // Restart the application to attempt migration again.
-                }
-                else if(args.Length > 0 && args[0] == "-retry")
-                {
-                    logger.LogError("Retry failed. Exiting.");
-                    return; // Exit the application if migration fails.
-                }
-            }
+            DatabaseMigrations(app, builder, args, logger);            
 
             logger.LogInformation($"Application Started: Running in {environment}");
 
@@ -281,6 +236,56 @@ namespace Msyu9Gates
                 app.Logger.LogError(ex, $"Error reading file {fileName}: {ex.Message}");
                 text = string.Empty;
                 return false;
+            }
+        }
+
+        private static void DatabaseMigrations(WebApplication app, WebApplicationBuilder builder, string[] args, ILogger logger)
+        {
+            try
+            {
+                using (var scope = app.Services.CreateScope())
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                    db.Database.Migrate(); // Applies existing migrations from the Migrations folder. Be sure to build and commit migrations before deploying.
+                                           // DO NOT COMMIT DATABASE FILES.  .db, .db-wal, and .db-shm (added to gitignore)
+                    app.Logger.LogInformation($"Database Migration Completed in: Running in {app.Environment}");
+                }
+                BuildGatesAndRegisterApis(app, builder);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred during database migration or data rebuild. Deleting Database file.");
+
+                string db_path = Path.Combine(app.Environment.ContentRootPath, "msyu9gates.db");
+                string db_wal_path = Path.Combine(app.Environment.ContentRootPath, "msyu9gates.db-wal");
+                string db_shm_path = Path.Combine(app.Environment.ContentRootPath, "msyu9gates.db-shm");
+
+                if (File.Exists(db_path))
+                {
+                    File.Delete(db_path);
+                    logger.LogWarning("Database file found: msyu9gates.db -- Deleting.");
+                }
+                if (File.Exists(db_wal_path))
+                {
+                    File.Delete(db_wal_path);
+                    logger.LogWarning("Database WAL file found: msyu9gates.db-wal -- Deleting.");
+                }
+                if (File.Exists(db_shm_path))
+                {
+                    File.Delete(db_shm_path);
+                    logger.LogWarning("Database SHM file found: msyu9gates.db-shm -- Deleting.");
+                }
+
+                if (args.Length == 0)
+                {
+                    logger.LogError("Retrying migration after deleting database files.");
+                    Main(new string[1] { "-retry" }); // Restart the application to attempt migration again.
+                }
+                else if (args.Length > 0 && args[0] == "-retry")
+                {
+                    logger.LogError("Retry failed. Exiting.");
+                    return; // Exit the application if migration fails.
+                }
             }
         }
 
