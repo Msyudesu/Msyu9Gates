d9bca7ce8458750fa4a9343a806e9911194aeba7
Msyu Desu <msyudesu@gmail.com>
2025-08-29 02:50:20 -0400

    msyu/redesign:  Added authentication/discord login and added some UI tweaks.

--- Msyu9Gates/Msyu9Gates/Program.cs
+++ Msyu9Gates/Msyu9Gates/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using AspNet.Security.OAuth.Discord;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
 
 using Msyu9Gates.Components;
 using Msyu9Gates.Data;
 using Msyu9Gates.Utils;
+using Msyu9Gates.Lib.Models;
 
 namespace Msyu9Gates;
 
@@ -28,10 +34,72 @@ public class Program
             options.UseSqlite(builder.Configuration.GetConnectionString("SQLite"));
         });
 
+        //Authentication
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = "Discord";
+        })
+        .AddCookie(options =>
+        {
+            options.LoginPath = "/login/discord";
+            options.LogoutPath = "/logout";
+            options.ExpireTimeSpan = TimeSpan.FromDays(30);
+            options.SlidingExpiration = true;
+        })
+        .AddDiscord("Discord", options =>
+        {
+            options.ClientId = builder.Configuration["Authentication:Discord:ClientId"]!;
+            options.ClientSecret = builder.Configuration["Authentication:Discord:ClientSecret"]!;
+            options.CallbackPath = "/signin-discord";
+            options.Scope.Clear();
+            options.Scope.Add("identify");
+            options.SaveTokens = true;
+
+            options.Events.OnCreatingTicket = async context =>
+            {
+                string discordId = context.Principal!.FindFirstValue(ClaimTypes.NameIdentifier)!;
+                string username = context.User.GetProperty("global_name").GetString() 
+                ?? context.User.GetProperty("username").GetString()!
+                ?? string.Empty;
+                string? avatar = context.User.TryGetProperty("avatar", out var avatarProperty) && avatarProperty.ValueKind != System.Text.Json.JsonValueKind.Null
+                    ? avatarProperty.GetString()
+                    : null;
+
+                // Save/Update User
+                var scopeFactory = context.HttpContext.RequestServices.GetRequiredService<IDbContextFactory<ApplicationDbContext>>();
+                await using var dbContext = await scopeFactory.CreateDbContextAsync();
+
+                var existing = await dbContext.UsersDb.FirstOrDefaultAsync(u => u.DiscordId == discordId);
+                if (existing is null)
+                {
+                    dbContext.UsersDb.Add(new UserModel
+                    {
+                        DiscordId = discordId,
+                        Username = username,
+                        Avatar = avatar,
+                        CreatedDateUtc = DateTimeOffset.UtcNow,
+                        LastLoginUtc = DateTimeOffset.UtcNow,
+                        IsActive = true
+                    });
+                }
+                else
+                {
+                    existing.Username = username;
+                    existing.Avatar = avatar;
+                    existing.LastLoginUtc = DateTimeOffset.UtcNow;
+                }
+                await dbContext.SaveChangesAsync();
+
+                ClaimsIdentity identity = (ClaimsIdentity)context?.Principal?.Identity!;
+                identity.AddClaim(new Claim("discord:avatar", avatar ?? string.Empty));
+            };
+        });
+
+        builder.Services.AddAuthorizationBuilder().AddPolicy("Authenticated", policy => policy.RequireAuthenticatedUser());
+
         var app = builder.Build();
-
         var logger = app.Services.GetRequiredService<ILogger<Program>>();
-
         var environment = app.Environment;
 
         DbUtils.DatabaseMigrations(app, builder, args, logger);            
@@ -52,6 +120,9 @@ public class Program
 
         app.UseHttpsRedirection();
 
+        app.UseAuthentication();
+        app.UseAuthorization();
+
         app.UseAntiforgery();
 
         app.MapStaticAssets();
@@ -59,6 +130,30 @@ public class Program
             .AddInteractiveServerRenderMode()
             .AddInteractiveWebAssemblyRenderMode()
             .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+        app.MapGet("/login/discord", (HttpContext context, string? returnUrl) =>
+        {
+            var properties = new AuthenticationProperties { RedirectUri = string.IsNullOrWhiteSpace(returnUrl ) ? "/" : returnUrl };
+            return Results.Challenge(properties, new[] { "Discord" });
+        });
+
+        app.MapPost("/logout", async(HttpContext context) =>
+        {
+            await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            context.Response.Redirect("/");
+        }).RequireAuthorization();
+
+        app.MapGet("/user/me", (ClaimsPrincipal user) =>
+        {
+            if (!user.Identity?.IsAuthenticated ?? true)
+                return Results.Unauthorized();
+            return Results.Ok(new
+            {
+                Id = user.FindFirstValue(ClaimTypes.NameIdentifier),
+                Username = user?.Identity?.Name ?? string.Empty,
+                Avatar = user?.FindFirstValue("discord:avatar") ?? string.Empty
+            });
+        });
 
         app.Run();
     }
