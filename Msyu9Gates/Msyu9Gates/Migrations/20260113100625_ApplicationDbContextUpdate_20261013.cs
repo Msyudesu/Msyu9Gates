47ed105ecc3929be1b2c63556c9dfbf1f33a6167
Msyu Desu <msyudesu@gmail.com>
2026-01-13 05:09:11 -0500

    msyu/redesign:  DB migrations

---
+++ Msyu9Gates/Msyu9Gates/Migrations/20260113100625_ApplicationDbContextUpdate_20261013.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Msyu9Gates.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationDbContextUpdate_20261013 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DiffiutyLevel",
+                table: "ChaptersDb",
+                newName: "DifficultyLevel");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DifficultyLevel",
+                table: "ChaptersDb",
+                newName: "DiffiutyLevel");
+        }
+    }
+}
